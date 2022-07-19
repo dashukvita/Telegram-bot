614fb77967aaa703e7e764d2581cb0b27df83234
Vitaliia Dashuk <vitaliia_dashuk@epam.com>
2022-07-19 17:59:28 +0200

    first step to install telegram-bot

--- src/main/java/com/dash/telegram/bot/Bot.java
+++ src/main/java/com/dash/telegram/bot/Bot.java
@@ -5,6 +5,7 @@ import lombok.EqualsAndHashCode;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -21,16 +22,25 @@ import java.util.Optional;
 public class Bot extends TelegramLongPollingBot {
 
     @Value("${bot.name}")
-    private String botUsername="@forecasting_scheduling_bot";
+    private String botUsername="@ForScheduling";
 
     @Value("${bot.token}")
-    private String botToken="";
+    private String botToken="5539086041:AAFwpm-8MriszV48UGU5jASf-IGeCaXz_X8";
 
     @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage()) {
-            handleMessage(update.getMessage());
+//            handleMessage(update.getMessage());
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                execute(
+                        SendMessage.builder()
+                                .chatId(message.getChatId().toString())
+                                .text("Hello world from Postman2")
+                                .build());
+            }
+            
         }
     }
 
@@ -55,7 +65,8 @@ public class Bot extends TelegramLongPollingBot {
     @SneakyThrows
     public static void main(String[] args) {
         Bot bot = new Bot();
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        telegramBotsApi.registerBot(bot);
+        bot.execute(SendMessage.builder().chatId("227982080").text("Hello world from Postman2").build());
+//        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+//        telegramBotsApi.registerBot(bot);
     }
 }
